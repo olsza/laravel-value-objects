f8334cfa2f8f50b086ff195c4b681824856aefef
Olsza <olsza@users.noreply.github.com>
2022-02-09 17:46:27 +0100

    add new files draft

---
+++ tests/Pest.php
@@ -0,0 +1,5 @@
+<?php
+
+use Olsza\ValueObjects\Tests\TestCase;
+
+uses(TestCase::class)->in(__DIR__);
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,10 +1,36 @@
 <?php
 
-namespace Tests;
+namespace Olsza\ValueObjects\Tests;
 
-use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Orchestra\Testbench\TestCase as Orchestra;
+//use Olsza\TaxNumber\TaxNumberServiceProvider;
 
-abstract class TestCase extends BaseTestCase
+class TestCase extends Orchestra
 {
-//    use CreatesApplication;
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        Factory::guessFactoryNamesUsing(
+            fn (string $modelName) => 'Olsza\\ValueObjects\\Database\\Factories\\'.class_basename($modelName).'Factory'
+        );
+    }
+
+    protected function getPackageProviders($app)
+    {
+        return [
+//            TaxNumberServiceProvider::class,
+        ];
+    }
+
+    public function getEnvironmentSetUp($app)
+    {
+        config()->set('database.default', 'testing');
+
+        /*
+        $migration = include __DIR__.'/../database/migrations/create_taxnumbervo_table.php.stub';
+        $migration->up();
+        */
+    }
 }
